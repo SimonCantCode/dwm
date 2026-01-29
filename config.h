f202182f457962b2ff2b36a29a18c62a258871c7
Simon <simondepomian@gmail.com>
2026-01-29 21:30:30 +0100

    added emoji and dictionary script etc.

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+// Path to this folder
+//#define DWM_PATH "/home/simon/Documents/Code/dwm"
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
@@ -49,9 +52,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact		= 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster		= 1;    /* number of clients in master area */
+static const int resizehints	= 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always spl    it vertically */
@@ -75,34 +78,36 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+// i get an error if i comment this out, too lazy to acctually fix.
 #define STATUSBAR "dwmblocks"
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static char dmenumon[2]			= "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]	= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]	= { "kitty", NULL };
 
 #include "shiftview.c"
 #include <X11/XF86keysym.h>
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+//	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+//	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+//	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_d,      spawn,		   SHCMD("/home/simon/Documents/Code/dwm/scripts/define") },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,	   zoom,           {0} },
 //	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,						XK_c,      killclient,     {0} },
-	{ Mod1Mask,						XK_F4,     killclient,     {0} },
+	{ Mod1Mask,						XK_F4,     killclient,     {0} }, // why not
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
+//	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,						XK_space,  togglefloating, {0} },
 //	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 //	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -124,8 +129,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		        XK_q,      quit,           {0} },
 	{ 0,							XK_Print,  spawn,		   {.v = (const char*[]){ "flameshot", "gui", NULL } } },
 	//{ 0,							XK_section,spawn,		   {.v = (const char*[]){ "xdotool", "type", "'🎀'", NULL } } },
-	//{ MODKEY,						XK_period, spawn,			SHCMD("xdotool type $(cat /Documents/Code/dwm/emoji | dmenu -i -l 10 | cut -d' ' -f1)") },
-	{ MODKEY,						XK_period,					spawn,	SHCMD("cat /Documents/Code/dwm/emoji | dmenu -i -l 10 >> mylog.log") },
+	{ MODKEY,						XK_period,					spawn,	SHCMD("xdotool type $(cat /home/simon/Documents/Code/dwm/scripts/emoji | dmenu -i -l 10 | cut -d' ' -f1)") },
 	{ 0,							XF86XK_MonBrightnessUp,		spawn,	SHCMD("brillo -q -A 5")},
 	{ 0,							XF86XK_MonBrightnessDown,	spawn,	SHCMD("brillo -q -U 5")},
 	{ 0,							XF86XK_AudioMute,			spawn,	SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
@@ -138,7 +142,7 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkButton,		0,		Button1,	spawn,		{.v = dmenucmd } },
+	{ ClkButton,			0,				Button1,		spawn,			{.v = dmenucmd } },
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
